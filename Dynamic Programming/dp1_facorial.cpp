99665bc475ce055d32e09b5694a5db39d568b1a8
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-08 01:09:39 +0530

    Update dp1_facorial.cpp

--- Dynamic Programming/dp1_facorial.cpp
+++ Dynamic Programming/dp1_facorial.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+//memorization
+//tc- O(n) 
+//sc - O(n) + O(n)
+int fn(int n, vector<int> &dp)
+{
+  if (n <= 1)
+    return n;
+  if (dp[n] != -1)
+    return dp[n];                               // one step
+  return dp[n] = fn(n - 1, dp) + fn(n - 2, dp); // two step
+}
 
+int fn(int n, vector<int> &dp)
+{
+  if (n <= 1)
+    return n;
+  if (dp[n] != -1)
+    return dp[n];                               // one step
+  return dp[n] = fn(n - 1, dp) + fn(n - 2, dp); // two step
+}
 int main()
 {
+  int n;
+  cin >> n;
+  // vector<int> dp(n + 1, -1);   //declarations
+  // cout << fn(n, dp);
+
+  //tabulation
+  //tc - O(n)
+  //sc - O(1)
+  int prev2 = 0;
+  int prev1 = 1;
+  for (int i = 2; i <= n; i++)
+  {
+    int curr = prev2 + prev1;
+    prev2 = prev1;
+    prev1 = curr;
+  }
+
+  cout << prev1 << endl;
 }
