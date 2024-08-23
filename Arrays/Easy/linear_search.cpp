df7348c1e10a6d05bba2f2b625a5cd4a22014ac3
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-23 19:00:33 +0530

    Update and rename linear_search.cpp to Arrays/Easy/linear_search.cpp

---
+++ Arrays/Easy/linear_search.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool linear_search(vector<int>arr, int n, int target){
+  for(int i = 0; i<n; i++) if(arr[i]==target)  return true;
+
+  return false;
+}
+int main(){
+  int n;
+  cin>>n;
+  vector<int>arr;
+  int target;
+  cin>>target;
+  for(int i=0; i<n; i++) cin>>arr[i];
+  if(linear_search(arr, n, target)) cout<<"found the target";
+  else cout<<"Not found the target";
+}
