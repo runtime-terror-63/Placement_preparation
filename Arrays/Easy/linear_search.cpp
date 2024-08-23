68c617ede55d9e9d4b5a7822b86f13719f48d85f
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-23 19:03:55 +0530

    linear_search.cpp🧑🏽‍💻

--- Arrays/Easy/linear_search.cpp
+++ Arrays/Easy/linear_search.cpp
@@ -1,6 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Example 1:
+Input: arr[]= 1 2 3 4 5, num = 3
+Output: 2
+Explanation: 3 is present in the 2nd index
+
+Example 2:
+Input: arr[]= 5 4 3 2 1, num = 5
+Output: 0
+Explanation: 5 is present in the 0th index
+*/
+
 //TC - O(n)
 //SC - O(1)
 bool linear_search(vector<int>arr, int n, int target){
