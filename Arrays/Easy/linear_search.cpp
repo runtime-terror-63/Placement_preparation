cd73842ffc424b468eb3e7b5e97b32ef5856b8f0
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-23 19:01:07 +0530

    Update linear_search.cpp

--- Arrays/Easy/linear_search.cpp
+++ Arrays/Easy/linear_search.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//TC - O(n)
+//SC - O(1)
 bool linear_search(vector<int>arr, int n, int target){
   for(int i = 0; i<n; i++) if(arr[i]==target)  return true;
 
