fcb2328ca00927cb7c2f6efae19649f08d5dfc91
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-24 18:33:08 +0530

    Update Sort_array_Os_1s_2s.cpp🔥brute->>optimal->>algorithm

--- Arrays/Medium/Sort_array_Os_1s_2s.cpp
+++ Arrays/Medium/Sort_array_Os_1s_2s.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Bruteforce approch
+// Tc - O(n2)
+// SC - O(1)
 void sort_array(vector<int> arr, int n)
 {
   int cnt0 = 0, cnt1 = 0, cnt2 = 0;
@@ -22,8 +25,37 @@ void sort_array(vector<int> arr, int n)
     arr[i] = 2;
 
   for (int i = 0; i < n; i++)
-    cout << arr[i];
+    cout << arr[i] << " ";
 }
+
+// DETUCH NATIONAL FLAG ALGORITHM
+// TC - O(N)
+// SC - O(1)
+
+void sort_array_Os_1s_2s(vector<int> arr, int n)
+{
+  int low = 0, mid = 0, high = n - 1;
+  while (mid <= high)
+  {
+    if (arr[mid] == 0)
+    {
+      swap(arr[low], arr[mid]);
+      low++;
+      mid++;
+    }
+    else if (arr[mid] == 1)
+      mid++;
+    else
+    {
+      swap(arr[mid], arr[high]);
+      high--;
+    }
+  }
+
+  for (auto it : arr)
+    cout << it << " ";
+}
+
 int main()
 {
   int n;
@@ -31,5 +63,5 @@ int main()
   vector<int> arr(n);
   for (int i = 0; i < n; i++)
     cin >> arr[i];
-  sort_array(arr, n);
+  sort_array_Os_1s_2s(arr, n);
 }
