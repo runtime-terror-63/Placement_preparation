50e52557f074565ac505d92409f93391e726577b
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-24 08:47:35 +0530

    Update Find_the_number_appear_ones_other_twice.cpp

--- Arrays/Easy/Find_the_number_appear_ones_other_twice.cpp
+++ Arrays/Easy/Find_the_number_appear_ones_other_twice.cpp
@@ -1,8 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Tc - O(n) +  O(n) + O(n)
-// Sc - O(n)
+// Bruteforce solution
+//Tc - O(2n)
+//Sc - O(n)
+int number_appear_ones(vector<int> arr, int n)
+{
+  int curr = 0;
+  for (int i = 0; i < n; i++)
+  {
+    curr = arr[i];
+    int cnt = 0;
+    for (int j = 0; j < n; j++)
+    {
+      if (arr[j] == curr)
+        cnt++;
+    }
+
+    if (cnt == 1)
+      return curr;
+  }
+  return -1;
+}
+
+// Hasing
+//  Tc - O(n) +  O(n) + O(n)
+//  Sc - O(n)
 int num_appear_ones(vector<int> arr, int n)
 {
   int maxi = *max_element(arr.begin(), arr.end());
@@ -22,15 +45,15 @@ int num_appear_ones(vector<int> arr, int n)
 }
 
 // mapping
-// TC - O(nlogn) + O(n)
-// Sc - O(m)
+// TC - O(nlogM) + O((n/2)+1)
+// Sc - O((n/2)+1)
 int num_ones_apear(vector<int> arr, int n)
 {
   map<int, int> mpp;
   int maxi = 0;
-  for (auto it : arr)
+  for (auto it : arr) //  O(nlogM)
     mpp[it]++;
-  for (auto it : mpp)
+  for (auto it : mpp) // O((n/2)+1)
   {
     if (it.second == 1)
       maxi = it.first;
@@ -39,6 +62,8 @@ int num_ones_apear(vector<int> arr, int n)
 }
 
 // Xor Operation
+// Tc - O(n)
+// Sc - O(1)
 int num_ones(vector<int> arr, int n)
 {
   int xorr = 0;
@@ -54,5 +79,5 @@ int main()
   vector<int> arr(n);
   for (int i = 0; i < n; i++)
     cin >> arr[i];
-  cout << num_ones(arr, n);
+  cout << number_appear_ones(arr, n);
 }
