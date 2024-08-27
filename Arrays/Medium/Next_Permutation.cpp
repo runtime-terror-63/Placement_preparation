66cb85a6c51f8657537aadfb6ee9dd9759dfec9f
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-27 19:03:55 +0530

    Update Next_Permutation.cpp

--- Arrays/Medium/Next_Permutation.cpp
+++ Arrays/Medium/Next_Permutation.cpp
@@ -9,8 +9,7 @@ int main()
 {
   int arr[] = {1, 3, 2};
 
-  next_permutation(arr, arr + 3); // using in-built function of C++
-
+  next_permutation(arr, arr + 3); 
   cout << arr[0] << " " << arr[1] << " " << arr[2];
 
   return 0;
